946fa91aa00c09b172a4499ed2b85fdb62d4e712
Daniel Rojas <jrojasm@est.utn.ac.cr>
2022-04-25 16:57:58 -0600

    Applying changes in function to use components

--- obsidiam-test/src/app/app.module.ts
+++ obsidiam-test/src/app/app.module.ts
@@ -4,9 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ConverterComponent } from './components/converter/converter.component';
+import { FooterComponent } from './components/footer/footer.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    ConverterComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
